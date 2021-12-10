80e78af7a705af08fb2b47839781ea7e06b717d1
VIctor <colen.victor@icloud.com>
2021-12-09 22:06:15 -0300

    Initial Commit

---
+++ Education Valley/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Education Valley
+//
+//  Created by Victor Colen on 09/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Education Valley/Education_ValleyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Education_ValleyApp.swift
+//  Education Valley
+//
+//  Created by Victor Colen on 09/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Education_ValleyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
